f9f124ed5440fdd4c35f5b481826ec43f7e57759
sandipanghos <sandipanghosh64@gmail.com>
2022-05-04 21:46:33 +0530

    adjustments on css/html and js

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
+/**
+ * Hide all tab contents
+ * Apply Default style all tabs
+ * current tab content show and tab to active style
+ * @param {*} evt
+ * @param {*} tabIndex
+ */
 function openTab(evt, tabIndex) {
-  var i, tabcontent, tablinks;
+  hideAllTabContents(evt, tabIndex);
+  deactivareAllTablinks(evt, tabIndex);
+  showSelectedTabContent(evt, tabIndex);
+}
+
+/**
+ * Hide all contents
+ * @param {*} evt
+ * @param {*} tabIndex
+ */
+function hideAllTabContents(evt, tabIndex) {
+  let i, tabcontent;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks");
+}
+
+/**
+ * current tab content show and tab to active style
+ * @param {*} evt
+ * @param {*} tabIndex
+ */
+function showSelectedTabContent(evt, tabIndex) {
+  document.getElementById(tabIndex).style.display = "block";
+  evt.target.className += " active";
+}
+
+/**
+ * Apply Default style all tabs
+ * @param {*} evt
+ * @param {*} tabIndex
+ */
+function deactivareAllTablinks(evt, tabIndex) {
+  let i, tablinks;
+  tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(tabIndex).style.display = "block";
-  evt.currentTarget.className += " active";
+}
+
+/**
+ * hide current tab content and  Apply Default style all tabs
+ * @param {*} evt
+ * @param {*} tabIndex
+ */
+function closeTab(evt, tabIndex) {
+  document.getElementById(tabIndex).style.display = "none";
+  deactivareAllTablinks(evt, tabIndex);
 }
